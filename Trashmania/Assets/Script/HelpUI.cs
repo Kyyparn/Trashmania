aaaf70c117572f7410df56bb570948887dd632bc
Parrexion <schack-93@hotmail.com>
2019-11-23 12:42:24 +0100

    Added Menu scenes and HUD

---
+++ Trashmania/Assets/Script/HelpUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpUI : MonoBehaviour {
+
+	[Header("UI components")]
+	public GameObject menuObject;
+
+	private bool visible;
+
+
+	private void Start() {
+		UpdateUI(false);
+		UIDelegator.instance.onShowHelp += UpdateUI;
+	}
+
+	private void UpdateUI(bool state) {
+		visible = state;
+		menuObject.SetActive(visible);
+	}
+
+	public void ToggleMenu() {
+		UpdateUI(!visible);
+	}
+}
---
+++ Trashmania/Assets/Script/UI/UIDelegator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDelegator : MonoBehaviour {
+
+	public static UIDelegator instance = null;
+	private void Awake() {
+		if (instance != this) {
+			instance = this;
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
+
+
+	public delegate void UpdateScore(int score);
+	public UpdateScore onScoreChange;
+
+	public delegate void UpdateTime(int days, int currentTime);
+	public UpdateTime onUpdateTime;
+
+	public delegate void UpdateHealth(int entityID, int health);
+	public UpdateHealth onUpdateHealth;
+
+	public delegate void ShowScreen(bool state);
+	public ShowScreen onShowPause;
+	public ShowScreen onShowHelp;
+
+}
