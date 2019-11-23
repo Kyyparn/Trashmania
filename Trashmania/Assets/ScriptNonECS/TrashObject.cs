da6e462dc0062d9f7645102c0e5371594d50cace
Parrexion <schack-93@hotmail.com>
2019-11-23 14:25:05 +0100

    It's alive!

---
+++ Trashmania/Assets/ScriptNonECS/TrashObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashObject : MonoBehaviour {
+
+	private void Start() {
+		GetComponent<Rigidbody>().AddForce(new Vector3(100f,0f,0f));
+		transform.localRotation = Quaternion.Euler(new Vector3(0,Random.Range(-180f,180f), 0f));
+	}
+
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag != "Oven") {
+			return;
+		}
+
+		Destroy(gameObject);
+	}
+}
