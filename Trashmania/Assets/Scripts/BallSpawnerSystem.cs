b288e9e7db2272932e7753e5f6b1d745261f0707
TobiasLuckey <tobias.luckey@gmail.com>
2019-11-23 11:50:31 +0100

    merge chaos already

---
+++ Trashmania/Assets/Scripts/BallSpawnerSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Rendering;
+
+public class BallSpawnerSystem : ComponentSystem
+{
+	EntityArchetype ball;
+
+	protected override void OnCreate()
+	{
+		ball = EntityManager.CreateArchetype(typeof(RenderMesh), typeof(Mesh), typeof(Material));
+		EntityManager.CreateEntity(ball);
+	}
+
+	protected override void OnUpdate()
+	{
+		
+	}
+}
