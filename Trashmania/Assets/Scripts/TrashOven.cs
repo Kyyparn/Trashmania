19c31dcb29c30f51c45c9d7186715edf0506e3fe
Simon Lundmark <simon.j.lundmark@gmail.com>
2019-11-23 21:41:25 +0100

    WIP

--- Trashmania/Assets/Scripts/TrashOven.cs
+++ Trashmania/Assets/Scripts/TrashOven.cs
@@ -1,11 +1,14 @@
 ﻿using Scripts.Enum;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashOven : MonoBehaviour {
 	const float MAX_HEALTH = 100;
+
     const string TRASH_TAG = "Trash";
+    const string PLAYER_TAG = "Player";
 
     [Header("Oven info")]
     [SerializeField] protected int entityID;
@@ -14,14 +17,37 @@ public class TrashOven : MonoBehaviour {
     [Header("Oven health")]
     [SerializeField] protected int health;
     [SerializeField] protected int damageOnFail;
-    [SerializeField] protected bool broken;
+    [SerializeField] protected bool isBroken;
+
+    [Header("Oven repair")]
+    [SerializeField] protected int healthPerTick;
+    [SerializeField] protected float tickTimeInSeconds;
+    [SerializeField] protected int repairHealthThreshold;
+
+    protected bool inRepairRange = false;
+    protected bool isRepairing = false;
+    protected float currentRepairTime = 0f;
 
     private void Start() {
+        health = (int)MAX_HEALTH;
         UIDelegator.instance.onUpdateHealth?.Invoke(entityID, health/MAX_HEALTH);
     }
 
+    private void FixedUpdate() {
+        if (isRepairing) {
+            float newRepairTime = currentRepairTime + Time.fixedDeltaTime;
+
+            if (newRepairTime >= tickTimeInSeconds) {
+                Heal();
+                newRepairTime -= tickTimeInSeconds;
+            }
+
+            currentRepairTime = newRepairTime;
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == TRASH_TAG && !broken) {
+        if (other.gameObject.tag == TRASH_TAG && !isBroken) {
             if (other.gameObject.GetComponent<TrashObject>()?.GetTrashType() == acceptedTrash) {
                 PlayerStats.instance.AddCorrectSortedTrash(other.gameObject.GetComponent<TrashObject>().GetTrashType());
             }
@@ -38,10 +64,27 @@ public class TrashOven : MonoBehaviour {
 
         if (health <= 0) {
             health = 0;
-            broken = true;
+            isBroken = true;
             print($"OVEN {entityID} is BROKEN!");
         }
 
         UIDelegator.instance.onUpdateHealth?.Invoke(entityID, health / MAX_HEALTH);
+    }
+
+    private void Heal() {
+        health += healthPerTick;
+
+        if (isBroken) {
+            if (health >= repairHealthThreshold) {
+                isBroken = false;
+            }
+        }
+
+        UIDelegator.instance.onUpdateHealth?.Invoke(entityID, health);
+    }
+
+    public void UpdateRepairState(bool isRepairing) {
+        this.isRepairing = isRepairing;
+        currentRepairTime = 0f;
     }
 }
