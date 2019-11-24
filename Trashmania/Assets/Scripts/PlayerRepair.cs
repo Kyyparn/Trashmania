e95efc5dbfa67093765f4068474611340ee658ac
TobiasLuckey <tobias.luckey@gmail.com>
2019-11-24 15:19:04 +0100

    Add repair animations

--- Trashmania/Assets/Scripts/PlayerRepair.cs
+++ Trashmania/Assets/Scripts/PlayerRepair.cs
@@ -6,42 +6,20 @@ public class PlayerRepair : MonoBehaviour
 {
     const string REPAIR_TRIGGER_TAG = "RepairTrigger";
 
-    TrashOven repairOvenRef = null;
+    public TrashOven repairOvenRef = null;
+	bool isTryingToRepair = false;
 
-    public void UpdateRepairState(bool repairState) {
-        repairOvenRef?.UpdateRepairState(repairState);
+    public void UpdateRepairState(bool isTryingToRepair) {
+		this.isTryingToRepair = isTryingToRepair;
     }
 
-    private void OnRepairRangeEnter(TrashOven oven) {
-        if (!repairOvenRef) {
-            repairOvenRef = oven;
-        }
-    }
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == REPAIR_TRIGGER_TAG)
+		{
+			repairOvenRef = other.gameObject.transform.parent.GetComponent<TrashOven>();
 
-    private void OnRepairRangeExit(TrashOven oven) {
-        if (repairOvenRef != null) {
-            if (repairOvenRef.Equals(oven)) {
-                repairOvenRef.UpdateRepairState(false);
-                repairOvenRef = null;
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == REPAIR_TRIGGER_TAG) {
-            TrashOven oven = other.gameObject.transform.parent.GetComponent<TrashOven>();
-
-            if (oven)
-                OnRepairRangeEnter(oven);
-        }
-    }
-
-    private void OnTriggerExit(Collider other) {
-        if (other.gameObject.tag == REPAIR_TRIGGER_TAG) {
-            TrashOven oven = other.gameObject.transform.parent.GetComponent<TrashOven>();
-
-            if (oven)
-                OnRepairRangeExit(oven);
-        }
-    }
+			repairOvenRef?.UpdateRepairState(isTryingToRepair);
+		}
+	}
 }
