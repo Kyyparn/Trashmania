9824de4491fa3ee83f10edec363969cd05acd5e5
Simon Lundmark <simon.j.lundmark@gmail.com>
2019-11-24 10:37:01 +0100

    Added repairtrigger and stuff

--- Trashmania/Assets/Scripts/PlayerRepair.cs
+++ Trashmania/Assets/Scripts/PlayerRepair.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerRepair : MonoBehaviour
 {
+    const string REPAIR_TRIGGER_TAG = "RepairTrigger";
+
     TrashOven repairOvenRef = null;
 
     public void UpdateRepairState(bool repairState) {
@@ -13,14 +15,33 @@ public class PlayerRepair : MonoBehaviour
     private void OnRepairRangeEnter(TrashOven oven) {
         if (!repairOvenRef) {
             repairOvenRef = oven;
-            print($"REPAIR RANGE ENTERED FOR {oven.name}");
         }
     }
 
     private void OnRepairRangeExit(TrashOven oven) {
-        if (repairOvenRef.Equals(oven)) {
-            repairOvenRef = null;
-            print($"REPAIR RANGE EXIT FOR {oven.name}");
+        if (repairOvenRef != null) {
+            if (repairOvenRef.Equals(oven)) {
+                repairOvenRef.UpdateRepairState(false);
+                repairOvenRef = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == REPAIR_TRIGGER_TAG) {
+            TrashOven oven = other.gameObject.transform.parent.GetComponent<TrashOven>();
+
+            if (oven)
+                OnRepairRangeEnter(oven);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == REPAIR_TRIGGER_TAG) {
+            TrashOven oven = other.gameObject.transform.parent.GetComponent<TrashOven>();
+
+            if (oven)
+                OnRepairRangeExit(oven);
         }
     }
 }
